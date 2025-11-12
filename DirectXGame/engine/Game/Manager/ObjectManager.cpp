d63d5253fdd303f0f49a066ca6b81cb55b6d1f8e
daichChiba <146914435+daichChiba@users.noreply.github.com.>
2025-11-12 12:17:13 +0900

    製作途中

---
+++ DirectXGame/engine/Game/Manager/ObjectManager.cpp
@@ -0,0 +1,25 @@
+#include "ObjectManager.h"
+#include "engine/Game/MapChip/MapChip.h"
+
+ObjectManager::ObjectManager() {
+	//
+}
+
+ObjectManager::~ObjectManager() {
+	//
+}
+
+void ObjectManager::Initilize(MapChip* mapchip) {
+	//
+	mapChipData_ = mapchip;
+
+
+}
+
+void ObjectManager::UpDate() {
+	//
+}
+
+void ObjectManager::Draw(KamataEngine::Camera* camera) {
+	//
+}
---
+++ DirectXGame/engine/Game/Manager/ObjectManager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "KamataEngine.h"
+
+class MapChip;
+
+class ObjectManager {
+public:
+	ObjectManager();
+	~ObjectManager();
+
+	void Initilize(MapChip* mapchip);
+	void UpDate();
+	void Draw(KamataEngine::Camera* camera);
+	void DrawImGui();
+	void Delete();
+
+private:
+	MapChip* mapChipData_;
+};
